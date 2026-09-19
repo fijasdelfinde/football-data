7dba1402854ef557e25516be199df297656a900d
fijasdelfinde <fijasdelfinde@gmail.com>
2026-09-19 00:15:54 -0500

    Update load_sheets.py

--- load_sheets.py
+++ load_sheets.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+
 def connect_sheets(service_file, spreadsheet_id):
 
     scopes = [
@@ -8,10 +9,16 @@ def connect_sheets(service_file, spreadsheet_id):
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = Credentials.from_service_account_file(service_file, scopes=scopes)
+    creds = Credentials.from_service_account_file(
+        service_file,
+        scopes=scopes
+    )
 
     gc = gspread.authorize(creds)
 
     sh = gc.open_by_key(spreadsheet_id)
 
-    return sh.worksheet("Matches"), sh.worksheet("Stats")
+    return (
+        sh.worksheet("Matches"),
+        sh.worksheet("Stats")
+    )
